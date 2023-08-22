31fe6a2c8e6311b0898030d28be365a25fd6926b
Raunak <raunakgiri21@gmail.com>
2023-08-22 22:44:14 +0530

    [Frontend] MainTweet & Explore Page

--- client/src/components/ExploreTweets/ExploreTweets.jsx
+++ client/src/components/ExploreTweets/ExploreTweets.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-const ExxploreTweets = () => {
-  return <div>ExxploreTweets</div>;
+import Tweet from "../Tweet/Tweet";
+
+const ExploreTweets = () => {
+  const [explore, setExplore] = useState();
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const exploreTweets = await axios.get(
+          `http://localhost:8000/api/tweets/explore`
+        );
+        setExplore(exploreTweets?.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [currentUser]);
+  return (
+    <div className="mt-6">
+      {explore &&
+        explore.map((tweet) => {
+          return (
+            <div key={tweet._id} className="p-2">
+              <Tweet tweet={tweet} setData={setExplore} />
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
-export default ExxploreTweets;
+export default ExploreTweets;
--- client/src/components/LeftSidebar/LeftSidebar.jsx
+++ client/src/components/LeftSidebar/LeftSidebar.jsx
@@ -5,10 +5,12 @@ import TagIcon from "@mui/icons-material/Tag";
 import PersonIcon from "@mui/icons-material/Person";
 import { Link } from "react-router-dom";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/userSlice";
 
 const LeftSidebar = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   const dispatch = useDispatch();
 
   const handleLogout = async (e) => {
@@ -34,7 +36,7 @@ const LeftSidebar = () => {
             <p>Explore</p>
           </div>
         </Link>
-        <Link to="/profile/">
+        <Link to={`/profile/${currentUser._id}`}>
           <div className="flex items-center space-x-6 px-2 py-2 hover:bg-slate-200 rounded-full">
             <PersonIcon fontSize="large" />
             <p>Profile</p>
@@ -43,8 +45,8 @@ const LeftSidebar = () => {
       </div>
       <div className="flex justify-between">
         <div>
-          <p className="font-bold">Username</p>
-          <p className="font-bold">@Username</p>
+          <p className="font-bold">{currentUser.username}</p>
+          <p className="font-bold">@{currentUser.username}</p>
         </div>
         <div>
           <Link to="/signin">
